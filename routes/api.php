9ca70fc8903d7a7327e19061e7a38b286a22d6d9
zaxwebs <zifidigital@gmail.com>
2024-09-13 21:54:33 +0530

    added hello route

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/hello', function (Request $request) {
+	return 'Hello World';
+});
+
