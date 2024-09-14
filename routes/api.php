0c24e1fbba89af96a543fa18e31dc12a49ad203f
zaxwebs <zifidigital@gmail.com>
2024-09-14 20:24:05 +0530

    WIP

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RocketController;
+use App\Http\Controllers\MissionController;
+
+Route::get('/hello', function (Request $request) {
+	return response()->json([
+		'success' => true,
+		'data' => 'Hello World!'
+	], 200);
+});
 
 Route::get('/user', function (Request $request) {
 	return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/hello', function (Request $request) {
-	return 'Hello World';
-});
+
 
 Route::apiResource('rockets', RocketController::class);
+Route::apiResource('missions', MissionController::class);
 
