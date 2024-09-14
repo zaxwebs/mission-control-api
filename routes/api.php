9c0e49f3064f179d6795bf1483b80f7af2ad23b0
zaxwebs <zifidigital@gmail.com>
2024-09-15 04:31:17 +0530

    WIP

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\RocketController;
 use App\Http\Controllers\MissionController;
 use App\Http\Controllers\AuthController;
 
+// Demo routes on content types
 Route::get('/hello-text', function (Request $request) {
 	// text/html
 	return 'Hello World!';
@@ -27,19 +28,33 @@ Route::get('/hello-response', function (Request $request) {
 	], 200);
 });
 
-// authentication
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->group(function () {
-	Route::post('/logout', [AuthController::class, 'logout']);
-});
-
 Route::get('/user', function (Request $request) {
 	return $request->user();
 })->middleware('auth:sanctum');
 
 
-Route::apiResource('rockets', RocketController::class);
-Route::apiResource('missions', MissionController::class);
+// Route::apiResource('rockets', RocketController::class);
+// Route::apiResource('missions', MissionController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+	// Protected routes that require authentication
+	Route::post('/logout', [AuthController::class, 'logout']);
+	Route::post('/missions', [MissionController::class, 'store']);
+	Route::put('/missions/{mission}', [MissionController::class, 'update']);
+	Route::delete('/missions/{mission}', [MissionController::class, 'destroy']);
+
+	Route::post('/rockets', [RocketController::class, 'store']);
+	Route::put('/rockets/{rocket}', [RocketController::class, 'update']);
+	Route::delete('/rockets/{rocket}', [RocketController::class, 'destroy']);
+});
+
+// Public routes (accessible without authentication)
+Route::get('/missions', [MissionController::class, 'index']);
+Route::get('/missions/{mission}', [MissionController::class, 'show']);
+
+Route::get('/rockets', [RocketController::class, 'index']);
+Route::get('/rockets/{rocket}', [RocketController::class, 'show']);
 
